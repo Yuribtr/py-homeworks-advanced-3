2aee588b47b58b7d0cf8cb1140060dd9259be34b
Yuri <Yuribtr@users.noreply.github.com>
2020-12-09 23:34:26 +0300

    Fixed simple decorator

--- DecoratorLogger.py
+++ DecoratorLogger.py
@@ -19,7 +19,8 @@ def decorator_simple_logger(old_function):
     def new_function(*args, **kwargs):
         with open(file='simple_log.txt', mode='a+', encoding='utf8') as log_file:
             log_file.write(f'Function "{old_function.__name__}" started at {date.today()} {datetime.now().time()}\n')
-            log_file.write(f'Arguments is: {args}, {kwargs}\n\n')
-            something = old_function(*args, **kwargs)
-        return something
+            log_file.write(f'Arguments is: {args}, {kwargs}\n')
+            result = old_function(*args, **kwargs)
+            log_file.write(f'Result is: {result}\n\n')
+        return result
     return new_function
